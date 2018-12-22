65cd864f47068338c1eeb9ed05861637e2ad36e3
manymany.macarons <manymany.macarons@gmail.com>
2018-12-22 12:41:59 +0900

    멤버 어댑터 골격 완성

---
+++ app/src/main/java/kr/tjit/listview_20181222_02/adapters/MemberAdapter.java
@@ -0,0 +1,41 @@
+package kr.tjit.listview_20181222_02.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
+import kr.tjit.listview_20181222_02.R;
+import kr.tjit.listview_20181222_02.datas.Member;
+
+public class MemberAdapter extends ArrayAdapter<Member> {
+
+    Context mContext = null;
+    List<Member> mList = null;
+    LayoutInflater inf = null;
+
+    public MemberAdapter(Context context, List<Member> list) {
+        super(context, R.layout.member_list_item, list);
+
+        this.mContext = context;
+        this.mList = list;
+        this.inf = LayoutInflater.from(mContext);
+
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View row = convertView;
+
+        if (row == null) {
+            row = inf.inflate(R.layout.member_list_item, null);
+        }
+
+        return row;
+
+    }
+}
