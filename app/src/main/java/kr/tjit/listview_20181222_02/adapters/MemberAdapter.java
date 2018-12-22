65f0f4b0f451a13fd00bf712926976e93716a493
manymany.macarons <manymany.macarons@gmail.com>
2018-12-22 12:48:45 +0900

    어댑터에서 실제 멤버 데이터를 반영

--- app/src/main/java/kr/tjit/listview_20181222_02/adapters/MemberAdapter.java
+++ app/src/main/java/kr/tjit/listview_20181222_02/adapters/MemberAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -34,6 +35,14 @@ public class MemberAdapter extends ArrayAdapter<Member> {
         if (row == null) {
             row = inf.inflate(R.layout.member_list_item, null);
         }
+
+        TextView nameTxt = row.findViewById(R.id.nameTxt);
+        TextView addressTxt = row.findViewById(R.id.addressTxt);
+
+        Member data = mList.get(position);
+
+        nameTxt.setText(data.getName());
+        addressTxt.setText(data.getAddress());
 
         return row;
 
